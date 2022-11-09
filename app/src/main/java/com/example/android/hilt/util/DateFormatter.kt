68a508e423356a91f48cadb5266ebe266047e5b9
hyunjine <hyunjin_98_dev@naver.com>
2022-11-09 23:34:55 +0900

    refactor: Change from service locator to Hilt

--- app/src/main/java/com/example/android/hilt/util/DateFormatter.kt
+++ app/src/main/java/com/example/android/hilt/util/DateFormatter.kt
@@ -18,15 +18,18 @@ package com.example.android.hilt.util
 
 import android.annotation.SuppressLint
 import java.text.SimpleDateFormat
-import java.util.Date
+import java.util.*
+import javax.inject.Inject
 
 /**
  * String formatter for the log dates.
  */
-class DateFormatter {
+class DateFormatter @Inject constructor() {
 
     @SuppressLint("SimpleDateFormat")
-    private val formatter = SimpleDateFormat("d MMM yyyy HH:mm:ss")
+    private val formatter = SimpleDateFormat("d MMM yyyy HH:mm:ss").apply {
+        timeZone = TimeZone.getTimeZone("Asia/Seoul")
+    }
 
     fun formatDate(timestamp: Long): String {
         return formatter.format(Date(timestamp))
